80faa02557e9937d7e615727fbf37184acae9e76
scyrock <ciccio.cerrito@gmail.com>
2025-10-05 18:17:41 +0200

    Stable solver.
    Handle nacked: candidate, row, column, block.

---
+++ exercise/99_projects/01_sudoku/cellctrl.h
@@ -0,0 +1,110 @@
+typedef struct{
+    int value;          // Cell assigne value (0 if unknown)
+    int candidate;      // Cell valid candidate stores in binary format 
+                           // (1 if valid candidate)
+}sudoku_cell;
+typedef sudoku_cell sudoku_grid[SUDOKU_S][SUDOKU_S];
+typedef sudoku_grid* sudoku_grid_ptr;
+   
+// >>>>> Candidate operation <<<<<
+
+/* Set the candidate `value` in position [`row`][`col`] */
+void set_candidate(sudoku_grid_ptr s,int row, int col, int value){
+    (*s)[row][col].candidate |= (1<<value);
+}
+   
+/* Unset the candidate `value` in position [`row`][`col`] */
+void unset_candidate(sudoku_grid_ptr s, int row, int col, int value){
+    (*s)[row][col].candidate &= ~(1<<value);
+}
+
+/* Check if `value` is a valid candidate of `candidates`.*/
+int is_candidate(int candidates, int value){
+    if(candidates & (1<<value)) return 1;
+    return 0;
+}
+/* Return true if there is only one allowed candidate.*/
+int only_one_candidate(int n){
+    if ((n & (n - 1)) == 0) return 1;
+    return 0;
+}
+
+/* Check if a value is a valid candidate in a row.*/
+int cand_absent_in_row(sudoku_grid_ptr s, int col, int value){
+    for(int row=0; row<SUDOKU_S; row++){
+        if((*s)[row][col].value==value) return 0;
+    }
+    return 1;  
+}
+
+/* Check if a value is a valid candidate in a column.*/
+int cand_absent_in_col(sudoku_grid_ptr s, int row, int value){
+    for(int col=0; col<SUDOKU_S; col++){
+        if((*s)[row][col].value==value) return 0;
+    }
+    return 1;  
+}
+
+/* Check if a value is a valid candidate in a its cell.*/
+int cand_absent_in_block(sudoku_grid_ptr s, int row0, int col0, int value){
+    for(int i=0; i<SUDOKU_L; i++){
+        for(int j=0; j<SUDOKU_L; j++){
+            if((*s)[row0+i][col0+j].value==value) return 0;
+        }
+    }
+    return 1;  
+}
+/* Given a column `col` and a taget value `value` return the row `row_target` in 
+ * which `val` is a naked candidate. Otherwise return -1. */
+int row_candidate_position(sudoku_grid_ptr s, int col, int value){
+    int row_target = -1;
+    for(int row=0; row<SUDOKU_S; row++){
+        if(is_candidate((*s)[row][col].candidate,value)){     // `value` is candidate
+            // printf("Row %d - Col %d - Value %d\n", row, col, value);
+            if(row_target==-1) row_target=row;                 // first time in the col
+            else return -1;                                    // not naked
+        } 
+    }
+    return row_target;
+}
+
+/* Given a row `row` and a taget value `value` return the column `col_target` in 
+ * which `val` is a naked candidate. Otherwise return -1. */
+int col_candidate_position(sudoku_grid_ptr s, int row, int value){
+    int col_target = -1;
+    for(int col=0; col<SUDOKU_S; col++){
+        if(is_candidate((*s)[row][col].candidate,value)){     // `value` is candidate
+            // printf("Row %d - Col %d - Value %d\n", row, col, value);
+            if(col_target==-1) col_target=col;                 // first time in the col
+            else return -1;                                    // not naked
+        } 
+    }
+    return col_target;
+}
+/* Given a block `row0, col0` and a taget value `value` return the position 
+ * `row_target, col_target` in  which `val` is a naked candidate. 
+ * Otherwise return `-1 -1`. */
+int block_candidate_position(sudoku_grid_ptr s,int row0,  int col0, 
+                                int value, int *target){
+    for(int i=0; i<SUDOKU_L; i++){
+        for(int j=0; j<SUDOKU_L; j++){
+            // printf("%d, %d, %d\n", i,j,value);
+            if(is_candidate((*s)[row0+i][col0+j].candidate,value)){
+                if(target[0]==-1 && target[1]==-1){
+                    target[0] = row0+i;
+                    target[1] = col0+j;
+                }else return 0; 
+            }
+        }
+    }
+    return 1;
+}
+/* Transform binary candidate to decimal.*/
+int candidate_bin2dec(int bin){
+    int dec = 0;
+    while (bin >>= 1) {
+        dec++;
+    }
+    return dec;
+}
+
--- exercise/99_projects/01_sudoku/sudoku.c
+++ exercise/99_projects/01_sudoku/sudoku.c
@@ -4,39 +4,7 @@
 #define SUDOKU_S 9 
 #define SUDOKU_DIM SUDOKU_S*SUDOKU_S
 
-typedef struct{
-    int value;          // Cell assigne value (0 if unknown)
-    int candidate;      // Cell valid candidate stores in binary format 
-                        // (1 if valid candidate)
-}sudoku_cell;
-typedef sudoku_cell sudoku_grid[SUDOKU_S][SUDOKU_S];
-typedef sudoku_grid* sudoku_grid_ptr;
-
-// >>>>> Candidate operation <<<<<
-/* Set the candidate `val` in position [`row`][`col`] */ 
-void set_candidate(sudoku_grid_ptr s, int value,int row, int col){
-    (*s)[row][col].candidate |= (1<<value);
-}
-
-/* Unset the candidate `val` in position [`row`][`col`] */
-void unset_candidate(sudoku_grid_ptr s, int row, int col, int value){
-    (*s)[row][col].candidate &= ~(1<<value);
-}
-
-/* Return true if there is only one allowed candidate.*/
-int only_one_candidate(int n){
-    if ((n & (n - 1)) == 0) return 1; 
-    return 0;
-}
-
-/* Transform binary candidate to decimal.*/
-int candidate_bin2dec(int bin){
-    int dec = 0;
-    while (bin >>= 1) {
-        dec++;
-    }
-    return dec;
-}
+#include "cellctrl.h"
 // >>>>> Visualization <<<<<
 void separation_line(void){
     for(int i=0; i<SUDOKU_S; i++){
@@ -76,17 +44,15 @@ void print_binary(int n){
 
 // >>>>> Initialization <<<<<
 void init_sudoku(sudoku_grid_ptr s){
-    // https://sudoku.coach/en/play/400060001020038000310004050500000000038000709000000003000010875006903000700020000
     
-    /*
-    char sudoku_in[SUDOKU_DIM+1] = "400060001020038000310004050"
-                                   "500000000038000709000000003"
-                                   "000010875006903000700020000"; 
-    */
+    /* Solvable */
+    // char sudoku_in[SUDOKU_DIM+1] = "400060001020038000310004050500000000038000709000000003000010875006903000700020000";    
+    // char sudoku_in[SUDOKU_DIM+1] = "200900007805147002730208100679301500001589206580670031150402098427000605960015400"; 
+    char sudoku_in[SUDOKU_DIM+1] = "000031000040702009000090002000000600090008010120070090700000900006400005250000000";
     
-    char sudoku_in[SUDOKU_DIM+1] = "200900007805147002730208100679301500001589206580670031150402098427000605960015400"; 
-    // "000004009020000000004910760310570000000000080800040000030000907000300054078000020";
-    //"200900007805147002730208100679301500001589206580670031150402098427000605960015400";
+    /* Unsolvable */
+    // char sudoku_in[SUDOKU_DIM+1]= "001000008800009001500030000900008000070200000000750002000000097400076030030000040";
+    printf("The solved sudoku is: \n https://sudoku.coach/en/play/%s\n", sudoku_in); 
     int row = 0;
     int col = 0;
     for(int i=0; i<SUDOKU_DIM; i++){
@@ -128,7 +94,7 @@ void candidate(sudoku_grid_ptr s){
                 for(int i=0; i<=SUDOKU_S; i++){
                     if(valid_candidate(s,row,col,i)){
                         // printf("%d  ", i);
-                        set_candidate(s,i,row,col);
+                        set_candidate(s,row,col,i);
                     }
                 }
                 // printf("\n");
@@ -168,12 +134,9 @@ int naked_candidate (sudoku_grid_ptr s){
             //printf("%d\n\n", (*s)[row][col].candidate);
             if(only_one_candidate(n) && n>0){
                 int val = candidate_bin2dec(n);
-                //while (n >>= 1) {
-                //    val++;
-                //}
                 //print_binary((*s)[row][col].candidate);
                 //printf("%d", (*s)[row][col].candidate);
-                printf("Row %d - Col %d - Value %d\n", row+1, col+1, val);
+                printf("Naked Candidate: Row %d - Col %d - Value %d\n", row+1, col+1, val);
                 release(s,row,col,val);
                 //print_binary((*s)[0][1].candidate);
                 (*s)[row][col].candidate = 0;
@@ -188,11 +151,85 @@ int naked_candidate (sudoku_grid_ptr s){
 /* Assign a value if is present as a candidate only in a cell of a row. */
 int naked_column(sudoku_grid_ptr s){
     for(int col=0; col<SUDOKU_S; col++){
-        int row_candidate = 0;
-        for(int row=0; row<SUDOKU_S; row++){
-            continue;
+        for(int row_candidate=1; row_candidate<=SUDOKU_S; row_candidate++){
+            if(cand_absent_in_row(s,col,row_candidate)){
+                int row_target = row_candidate_position(s, col, row_candidate);
+                //printf("The returned value of row_candidate_position is: %d\n", row_target);
+                if (row_target>=0){
+                   //print_binary((*s)[row][col].candidate);
+                   //printf("%d", (*s)[row][col].candidate);
+                   printf("Naked Column   : Row %d - Col %d - Value %d\n", row_target+1, col+1, row_candidate);
+                   release(s,row_target,col,row_candidate);
+                   (*s)[row_target][col].candidate = 0;
+                   (*s)[row_target][col].value = row_candidate;
+                   return 1;
+                }
+            }
         }
     }
+    return 0;
+}
+
+/* Assign a value if is present as a candidate only in a cell of a column. */
+int naked_row(sudoku_grid_ptr s){
+    for(int row=0; row<SUDOKU_S; row++){
+        for(int col_candidate=1; col_candidate<=SUDOKU_S; col_candidate++){
+            if(cand_absent_in_col(s,row,col_candidate)){
+                int col_target = col_candidate_position(s, row, col_candidate);
+                //printf("The returned value of col_candidate_position is: %d\n", col_target);
+                if (col_target>=0){
+                   //print_binary((*s)[row][col].candidate);
+                   //printf("%d", (*s)[row][col].candidate);
+                   printf("Naked Row      : Row %d - Col %d - Value %d\n", row+1, col_target+1, col_candidate);
+                   release(s,row,col_target,col_candidate);
+                   (*s)[row][col_target].candidate = 0;
+                   (*s)[row][col_target].value = col_candidate;
+                   return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+/* Assign a value if is present as a candidate only in a cell of a block. */
+int naked_block(sudoku_grid_ptr s){
+    for(int row0=0; row0<SUDOKU_S; row0+=SUDOKU_L){
+        for(int col0=0; col0<SUDOKU_S; col0+=SUDOKU_L){
+            for(int block_candidate=1; block_candidate<=SUDOKU_S; block_candidate++){
+                if(cand_absent_in_block(s,row0,col0,block_candidate)){
+                    int target[2] = {-1,-1}; 
+                    //printf("Row 0 = %d, Col0 = %d, candidate = %d\n", row0, col0, block_candidate);
+
+                    //printf("AAA %d", flag);
+                    if(block_candidate_position(s,row0,col0, block_candidate,target)
+                            && target[0]>=0 && target[1]>=0){
+                        int row_target = target[0];
+                        int col_target = target[1];
+                        printf("Naked Block    : Row %d - Col %d - Value %d\n", 
+                                row_target+1, col_target+1, block_candidate);
+                        release(s,row_target,col_target,block_candidate);
+                        (*s)[row_target][col_target].candidate = 0;
+                        (*s)[row_target][col_target].value = block_candidate;
+                        return 1;
+                    } 
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int solve_sudoku(sudoku_grid_ptr s){
+    int updated = 0;
+    updated = (naked_candidate(s) | updated);
+    updated = (naked_column(s) | updated);
+    updated = (naked_row(s) | updated);
+    updated = (naked_block(s) | updated);
+    // printf("Updated: %d\n", updated);
+    // print_binary((*s)[2][4].candidate);
+
+    return updated;
 }
 
 int main(void){
@@ -200,11 +237,10 @@ int main(void){
     init_sudoku(&sudoku);
     display_sudoku(&sudoku);
     candidate(&sudoku);
-    int done = 1;
+    int updated = 1;
     
-    while(done) done = naked_candidate(&sudoku); 
-   
-    
+    while(updated) updated = solve_sudoku(&sudoku); 
+    //updated = solve_sudoku(&sudoku); 
     display_sudoku(&sudoku);
     return 0;
 }
